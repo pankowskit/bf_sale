8e856686238df7a8e83125e402c0d61490554fd1
tomaszpankowski <--get-all>
2022-05-22 22:35:30 +0200

    layout

--- src/views/submissions.js
+++ src/views/submissions.js
@@ -1,10 +1,21 @@
-import {Container} from "react-bootstrap";
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+import SubmissionsS1 from "../components/submissions-s1";
+import SubmissionsS2 from "../components/submissions-s2";
+import SubmissionsS3 from "../components/submissions-s3";
+import SubmissionsS4 from "../components/submissions-s4";
 
-function Submissions(){
-    return(
-        <Container fluid className="minh-footer-adj bg-dark d-flex align-items-center py-5">
-            Submissions
-        </Container>
-    );
+class Submissions extends Component{
+    render(){
+        return(        
+            <Container fluid className="minh-footer-adj p-0">
+                <SubmissionsS1/>
+                <SubmissionsS2/>
+                <SubmissionsS3/>
+                <SubmissionsS4/>
+            </Container>    
+        );
+    }
 }
+
 export default Submissions;
